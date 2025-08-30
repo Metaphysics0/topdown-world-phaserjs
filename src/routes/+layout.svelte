9e596995251389fc37558e7f1af39e83b394fb39
Ryan Roberts <ryanroberts562@gmail.com>
2025-08-30 19:55:52 +0300

    chore: remove html title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,12 @@
-<svelte:head>
-    <title>Phaser Svelte Template</title>
-</svelte:head>
-
 <slot />
 
 <style>
-    :global(body) {
-        background-color: #0cc9bd;
-        margin: 0;
-        padding: 0;
-        color: rgba(255, 255, 255, 0.87);
-        background-color: #000000;
-        font-family: Arial, Helvetica, sans-serif;
-    }
+  :global(body) {
+    background-color: #0cc9bd;
+    margin: 0;
+    padding: 0;
+    color: rgba(255, 255, 255, 0.87);
+    background-color: #000000;
+    font-family: Arial, Helvetica, sans-serif;
+  }
 </style>
