5e6c486ac71e10ac815eb1fcfc40ca72b77ca7a3
Ryan Roberts <ryanroberts562@gmail.com>
2025-08-30 16:30:22 +0300

    feat: refactor

--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -20,6 +20,27 @@ export class Game extends Scene {
     this.camera = this.cameras.main;
     this.camera.setBackgroundColor(0x00ff00);
 
+    this.createMap();
+    this.createPlayer();
+
+    EventBus.emit("current-scene-ready", this);
+  }
+
+  update() {
+    this.handleInput();
+  }
+
+  changeScene() {
+    this.scene.start("GameOver");
+  }
+
+  private createPlayer() {
+    this.player = this.physics.add.sprite(100, 100, "player");
+    this.player.setSize(15, 19);
+    this.player.anims.play("idle");
+  }
+
+  private createMap() {
     const map = this.make.tilemap({ key: "map" });
     const tileset = map.addTilesetImage(
       "overworld_tileset-again", // <-- name from your JSON
@@ -33,19 +54,9 @@ export class Game extends Scene {
 
     map.createLayer("Tile Layer 1", tileset, offsetX, offsetY);
     map.createLayer("Tile Layer 2", tileset, offsetX, offsetY);
-
-    this.player = this.physics.add.sprite(100, 100, "player");
-
-    // Adjust the physics body to match the sprite size from the texture atlas
-    this.player.setSize(15, 19);
-    this.player.setScale(2); // Scale up to make it more visible
-
-    this.player.anims.play("idle");
-
-    EventBus.emit("current-scene-ready", this);
   }
 
-  update() {
+  private handleInput() {
     const cursors = this.input.keyboard!.createCursorKeys();
     const speed = 100;
 
@@ -65,14 +76,13 @@ export class Game extends Scene {
       this.player.setVelocityY(speed);
     }
 
-    if (this.player.body.velocity.x !== 0 || this.player.body.velocity.y !== 0) {
+    if (
+      this.player.body.velocity.x !== 0 ||
+      this.player.body.velocity.y !== 0
+    ) {
       this.player.anims.play("walk", true);
     } else {
       this.player.anims.play("idle", true);
     }
-  }
-
-  changeScene() {
-    this.scene.start("GameOver");
   }
 }
